c4c4f6e5bbad3e3850e8535c1312729eb8c26a8c
TranTraiJr <tttrai98@gmail.com>
2024-01-24 18:41:31 +0700

    first commit

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,7 +7,6 @@ function Game() {
   const [winner, setWinner] = useState(null);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[currentMove];
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
